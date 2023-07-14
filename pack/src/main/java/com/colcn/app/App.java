c7b87a6ff48b528df8f4764cdc73f34540ebe573
durga1808 <durgalakshmi3713@gmail.com>
2023-07-14 12:27:53 +0530

    inserted

---
+++ pack/src/main/java/com/colcn/app/App.java
@@ -0,0 +1,13 @@
+package com.colcn.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
