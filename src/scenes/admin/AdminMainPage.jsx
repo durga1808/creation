8ffa34b870641d6687760f90ec838b4e383a8c71
Mariselvamchellaiya <91615624+Mariselvamchellaiya@users.noreply.github.com>
2024-01-11 16:46:06 +0530

    Admin password Shown details updated in table

--- src/scenes/admin/AdminMainPage.jsx
+++ src/scenes/admin/AdminMainPage.jsx
@@ -1,115 +1,3 @@
-// import React, { useState } from "react";
-// import { AppBar, Toolbar, Typography, Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, TextField } from '@mui/material';
-// import { loginUser } from "../../api/LoginApiService";
-// import { useNavigate } from "react-router-dom";
-
-// const AdminTopBar = () => {
-
-//   const navigate = useNavigate();
-//   const [editableRowId, setEditableRowId] = useState(null);
-//   // Mock data
-// const mockData = [
-//   { id: 1, userName: 'User1', clusterType: 'Type A', hostURL: 'http://example.com/1' },
-//   { id: 2, userName: 'User2', clusterType: 'Type B', hostURL: 'http://example.com/2' },
-//   { id: 1, userName: 'User1', clusterType: 'Type A', hostURL: 'http://example.com/1' },
-//   { id: 2, userName: 'User2', clusterType: 'Type B', hostURL: 'http://example.com/2' },
-//   { id: 1, userName: 'User1', clusterType: 'Type A', hostURL: 'http://example.com/1' },
-//   { id: 2, userName: 'User2', clusterType: 'Type B', hostURL: 'http://example.com/2' },
-
-//   // Add more mock data as needed
-// ];
-
-// const handleAddCluster = ()=>{
-//   navigate("addcluster");
-// }
-// const handleEditRow = (rowId) => {
-//   setEditableRowId(rowId);
-// };
-
-// const handleSaveRow = () => {
-//   // Implement logic to save the edited row data
-//   setEditableRowId(null);
-// };
-
-// const data ={
-//   username:"mariselvam",
-//   password:"Selvam@3799"
-// }
-// const handlelogin = loginUser(data);
-// console.log("handlelogin",handlelogin);
-
-//   return (
-//     <div>
-//       {" "}
-//       <AppBar position="static">
-//         <Toolbar sx={{ backgroundColor: "#091365" }}>
-//           <Typography
-//             variant="h4"
-//             component="div"
-//             sx={{ flexGrow: 1, color: "white", fontWeight: "bold" }}
-//           >
-//             Cluster
-//           </Typography>
-//           <Button
-//             sx={{
-//               backgroundColor: "gray",
-//               marginRight: "20px",
-//               "&:hover": { backgroundColor: "gray" },
-//             }}
-//           >
-//             Add Rule
-//           </Button>
-//           <Button
-//           onClick={handleAddCluster}
-//             sx={{
-//               backgroundColor: "gray",
-//               marginRight: "20px",
-//               "&:hover": { backgroundColor: "gray" },
-
-//             }}
-//           >
-//             Add Cluster
-//           </Button>
-//         </Toolbar>
-//       </AppBar>
-//       <TableContainer component={Paper} sx={{ marginTop: 4}}>
-//         <Table>
-//           <TableHead sx={{ backgroundColor: '#091365' }}>
-//             <TableRow>
-//               <TableCell align="center" sx={{color:"white"}}>User Name</TableCell>
-//               <TableCell align="center" sx={{color:"white"}}>Cluster Type</TableCell>
-//               <TableCell align="center" sx={{color:"white"}}>Host URL</TableCell>
-//               <TableCell align="center" sx={{color:"white"}}>Action</TableCell>
-//             </TableRow>
-//           </TableHead>
-//           <TableBody>
-//             {mockData.map((row) => (
-//               <TableRow key={row.id}>
-//                  <TableCell align="center">{editableRowId === row.id ? <TextField value={row.userName} /> : row.userName}</TableCell>
-//                 <TableCell align="center">{editableRowId === row.id ? <TextField value={row.clusterType} /> : row.clusterType}</TableCell>
-//                 <TableCell align="center">{editableRowId === row.id ? <TextField value={row.hostURL} /> : row.hostURL}</TableCell>
-//                 <TableCell align="center">
-//                   {editableRowId === row.id ? (
-//                     <Button variant="contained" color="primary" onClick={handleSaveRow}>
-//                       Save
-//                     </Button>
-//                   ) : (
-//                     <Button variant="contained" color="primary" onClick={() => handleEditRow(row.id)}>
-//                       Edit
-//                     </Button>
-//                   )}
-//                 </TableCell>
-//               </TableRow>
-//             ))}
-//           </TableBody>
-//         </Table>
-//       </TableContainer>
-//     </div>
-//   );
-// };
-
-// export default AdminTopBar;
-
 import React, { useEffect, useState } from "react";
 import {
   AppBar,
@@ -163,15 +51,7 @@ const AdminTopBar = () => {
     fetchData();
   }, [editableRowId]);
 
-  // useEffect( () => {
-  //   const userDetails = JSON.parse(localStorage.getItem("userInfo"));
-  //   const payload = {
-  //     username: userDetails.username,
-  //     password: userDetails.password,
-  //   };
-  //   const ClusterData = await loginUser(payload);
-  //   console.log("clusterData adminPage", ClusterData);
-  // }, []);
+
 
   const handleAddCluster = () => {
     navigate("addcluster");
@@ -220,13 +100,12 @@ const AdminTopBar = () => {
     password: "Selvam@3799",
   };
 
-  // const handlelogin = loginUser(data);
-  // console.log("handlelogin", handlelogin);
+  // 091365
 
   return (
     <div>
       <AppBar position="static">
-        <Toolbar sx={{ backgroundColor: "#091365" }}>
+        <Toolbar sx={{ backgroundColor: "midnightblue" }}>
           <Typography
             variant="h4"
             component="div"
@@ -257,7 +136,7 @@ const AdminTopBar = () => {
       </AppBar>
       <TableContainer component={Paper} sx={{ marginTop: 4 }}>
         <Table>
-          <TableHead sx={{ backgroundColor: "#091365" }}>
+          <TableHead sx={{ backgroundColor: "midnightblue" }}>
             <TableRow>
               <TableCell align="center" sx={{ color: "white" }}>
                 User Name
@@ -278,17 +157,6 @@ const AdminTopBar = () => {
           </TableHead>
           <TableBody>
             {ClusterData.map((row, index) => (
-          //   <TableRow key={row.clusterId}>
-          //   <TableCell align="center">{row.clusterUsername}</TableCell>
-          //   <TableCell align="center">{row.clusterType}</TableCell>
-          //   <TableCell align="center">{row.hostUrl}</TableCell>
-          //   <TableCell align="center">
-          //     {/* Add Action button or any other action element here */}
-          //     <Button variant="contained" color="primary">
-          //       Open
-          //     </Button>
-          //   </TableCell>
-          // </TableRow>
               <TableRow key={row.clusterId}>
                 <TableCell align="center">
                   {editableRowId === row.clusterId ? (
@@ -300,15 +168,15 @@ const AdminTopBar = () => {
                     row.clusterUsername
                   )}
                 </TableCell>
-
-                <TableCell align="center">
+                 <TableCell align="center">
                   {editableRowId === row.clusterId ? (
                     <TextField
+                      type="text"
                       value={editedPassword}
                       onChange={(e) => setEditedPassword(e.target.value)}
                     />
                   ) : (
-                    row.clusterPassword
+                    '*'.repeat(row.clusterPassword.length)
                   )}
                 </TableCell>
 
