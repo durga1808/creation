e57a5655e2c19780d39c48f9f8540d9a4f024c04
durga1808 <durgalakshmi3713@gmail.com>
2024-02-03 16:19:25 +0530

    not authorized msg display on admin page

--- src/global/PrivateRouter.jsx
+++ src/global/PrivateRouter.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+
+
+import React from 'react';
+import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
 
 const PrivateRouter = () => {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1); // This is equivalent to history.goBack()
+  };
+
   return (
-    <div>
-      <p>Not authorized</p>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+      <p style={{ fontWeight: 'bold', fontSize: 20 }}>Permission Denied: Access restricted to authorized users only.</p>
+      <Button variant="contained" color="primary" onClick={handleGoBack} style={{ marginTop: '10px' }}>
+        Go Back
+      </Button>
     </div>
-  )
+  );
 }
 
-export default PrivateRouter
+export default PrivateRouter;
+
